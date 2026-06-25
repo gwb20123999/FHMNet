5e653592e0e237b01ba3f6d1bb89829be6fc5562
gwb20123999 <132733171+gwb20123999@users.noreply.github.com>
2026-06-25 21:46:30 +0800

    Update infer.py

--- infer.py
+++ infer.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 import cv2
 import torch
 import torch.nn.functional as F
-from models.FHKMNet import FHKMNet
+from models.FHMNet import FHMNet
 from data import get_dataloader
 from utils.utils import load_model_params, set_gpu
 
@@ -33,14 +33,14 @@ def gen_maps(model, test_loader, target_dir):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_name', type=str, default='FHKMNet')
+    parser.add_argument('--model_name', type=str, default='FHMNet')
     parser.add_argument('--dim', type=int, default=64, help='dimension of our model')
     parser.add_argument('--imgsize', type=int, default=352, help='testing image size')
     parser.add_argument('--shot', type=int, default=5)
     parser.add_argument('--num_workers', type=int, default=8, help='the number of workers in dataloader')
     parser.add_argument('--gpu_id', type=str, default='0', help='train use gpu')
     parser.add_argument('--data_root', type=str, default='./dataset/R2C7K', help='the path to put dataset')
-    parser.add_argument('--save_root', type=str, default='./snapshot/fhkmnet', help='the path to save model params and log')
+    parser.add_argument('--save_root', type=str, default='./snapshot/fhmnet', help='the path to save model params and log')
     parser.add_argument('--pred_map_root', type=str, default='./pred_map/', help='the path to save pred maps')
     parser.add_argument('--pvt_weights', type=str, default='./pvt_weights/pvt_v2_b2.pth', help='the path to pretrained backbone weights')
 
@@ -49,7 +49,7 @@ if __name__ == '__main__':
 
     set_gpu(opt.gpu_id)
 
-    ref_model = FHKMNet(opt).cuda()
+    ref_model = FHMNet(opt).cuda()
     params_path = os.path.join(opt.save_root, opt.model_name, 'Net_epoch_45.pth')
     ref_model = load_model_params(ref_model, params_path)
 
